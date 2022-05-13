65ca8d4da10e3b1d91c5e73d7cd2a7ac7533215b
gofly <gofly@outlook.com>
2022-05-13 18:18:43 +0800

    npm publish

--- lib/test/rediskTest.ts
+++ lib/test/rediskTest.ts
@@ -1,13 +1,6 @@
-import { Redisjson } from "./redisjson";
-import { RedisClientOptions } from 'redis';
 import * as redis from 'redis';
 import Client from "../client";
 import { GroupItem, User } from "./User";
-
-// declare global {
-//     clientOM: Client;
-// }
-
 
 async function test() {
     let cli = redis.createClient();
@@ -23,13 +16,12 @@ async function test() {
         name: 'zhangsan',
         age: 100,
         created: new Date(),
-        dbs:["redis","json","redisjson"],
+        dbs: ["redis", "json", "redisjson"],
         // group: {
         //     groupName: 'xiaoxueGroup',
         //     score: 100,
         // }
     })
-
 
     // user.group = {
     //     groupName: 'xiaoxueGroup',
@@ -43,7 +35,7 @@ async function test() {
     let dbUsers = await User.repository.search().where('id').eq(100010).returnAll();
     dbUsers.forEach(async (item) => {
         console.log('dbUser=', item.toJSON())
-        item.age = Math.random()*1000000;
+        item.age = Math.random() * 1000000;
         await User.repository.save(item);
     })
     // console.log('dbUser=', dbUser.toJSON());
