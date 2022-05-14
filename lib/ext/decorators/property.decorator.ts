e5d29f9f1f081d5ab0c96860635458903b16d762
yng <yng@outlook.com>
2022-05-14 15:25:48 +0800

    优化

--- lib/ext/decorators/property.decorator.ts
+++ lib/ext/decorators/property.decorator.ts
@@ -1,11 +1,12 @@
 import { MetadataStorage } from '../metadata/metadata.storage';
 import 'reflect-metadata';
 import FieldDefinition from '../../schema/definition/field-definition';
+import SchemaFieldType from '../../schema/definition/schema-field-type';
 
 export function MetadataProperty(options: FieldDefinition): Function {
     return (object: object, propertyName: string) => {
         const reflectType = Reflect.getMetadata('design:type', object, propertyName);
-        let mtype: any = 'string';
+        let mtype: SchemaFieldType = 'string';
         if (options.type === 'object') {
             mtype = reflectType.name;
         } else {
